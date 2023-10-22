38aaf7e6b74b24fb78fb59ec67933a87c16e2de7
hehmdalolkek <badeha.inna@yandex.ru>
2023-10-22 14:01:08 +0400

    added new types of errors

---
+++ errors/bad-request.js
@@ -0,0 +1,12 @@
+const CustomAPIError = require('./custom-error');
+
+
+class BadRequest extends CustomAPIError {
+	constructor(message) {
+		super(message);
+		this.statusCode = 400;
+	}
+}
+
+
+module.exports = BadRequest;
